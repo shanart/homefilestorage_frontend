1e1e469d0d4fc99c95d65084360d1c2eb4f3a02a
shanart <shanin.x@gmail.com>
2023-02-14 03:08:55 +0200

    add ngrx store, devtools and router-store

---
+++ src/app/app.reducers.ts
@@ -0,0 +1,20 @@
+import { routerReducer } from "@ngrx/router-store";
+import { ActionReducer, ActionReducerMap, MetaReducer } from "@ngrx/store";
+import { environment } from "../environments/environment";
+
+
+export interface AppState {
+
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+    router: routerReducer
+}
+
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+    return (state, action) => {
+        return reducer(state, action);
+    }
+}
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production? [logger]: [];
