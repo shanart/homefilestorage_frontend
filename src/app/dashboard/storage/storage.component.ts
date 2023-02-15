c844b46cd24083deb99c6fe07cda6e4d8ed7ec9b
shanart <shanin.x@gmail.com>
2023-02-15 21:47:17 +0200

    fix querieng parent id

--- src/app/dashboard/storage/storage.component.ts
+++ src/app/dashboard/storage/storage.component.ts
@@ -6,34 +6,27 @@ import { StorageFile, StorageFolder } from './models';
 import { StorageService } from './services/storage.service';
 
 @Component({
-  selector: 'app-storage',
-  templateUrl: './storage.component.html',
-  styleUrls: ['./storage.component.scss']
+    selector: 'app-storage',
+    templateUrl: './storage.component.html',
+    styleUrls: ['./storage.component.scss']
 })
 export class StorageComponent implements OnInit {
 
-    parent: Observable<number|null> = null;
+    parent: number | null = null;
     files$: Observable<StorageFile[]>;
     folders$: Observable<StorageFolder[]>;
     activatedRoute: ActivatedRoute;
 
     constructor(private storageService: StorageService,
-                private route: ActivatedRoute,
-                private router: Router) { }
+        private route: ActivatedRoute,
+        private router: Router) { }
 
     ngOnInit(): void {
         this.route.queryParams.subscribe(params => {
-            console.log(params['parent']);
-            if (params['parent'])
-                this.parent = params['parent'];
+            this.parent = params['parent']? +params['parent']: null;
+            this.files$ = this.storageService.getFiles(this.parent);
+            this.folders$ = this.storageService.getFolders(this.parent);
         });
-
-        this.route.queryParamMap.subscribe(
-            data => data.get('parent')? this.parent = +data.get('parent'): this.parent = null
-        );
-        this.files$ = this.storageService.getFiles(this.parent.pipe);
-        this.folders$ = this.storageService.getFolders(this.parent);
-        this.activatedRoute = this.route;
     }
 
     onGetParent(id) {
